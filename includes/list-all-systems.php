2825309adc9e516932689ea1b2e809c089ea22a2
Master <master@Masters-MacBook-Pro.local>
2024-06-01 12:07:06 +0200

    more testing and same prpblem

--- includes/list-all-systems.php
+++ includes/list-all-systems.php
@@ -2,12 +2,20 @@
 /**
  * List All Systems Page
  */
+
 function qkn_list_all_systems_page() {
     ?>
     <div class="systems-wrapper">
         <h1>All Systems</h1>
         <form class="search-form" method="GET">
             <input type="text" name="title" placeholder="Search by title" value="<?php echo isset($_GET['title']) ? esc_attr($_GET['title']) : ''; ?>">
+            <select name="posts_per_page" onchange="this.form.submit()">
+                <option value="10" <?php selected(10, $_GET['posts_per_page'] ?? 10); ?>>10</option>
+                <option value="20" <?php selected(20, $_GET['posts_per_page'] ?? 10); ?>>20</option>
+                <option value="50" <?php selected(50, $_GET['posts_per_page'] ?? 10); ?>>50</option>
+                <option value="100" <?php selected(100, $_GET['posts_per_page'] ?? 10); ?>>100</option>
+                <option value="-1" <?php selected(-1, $_GET['posts_per_page'] ?? 10); ?>>All</option>
+            </select>
             <button type="submit">Search</button>
         </form>
         <table id="systemsTable" class="systems-table">
@@ -26,51 +34,45 @@ function qkn_list_all_systems_page() {
             </thead>
             <tbody>
                 <?php
+                $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+                $posts_per_page = isset($_GET['posts_per_page']) ? (int) $_GET['posts_per_page'] : 10;
+
                 $args = array(
                     'post_type' => 'system',
-                    'posts_per_page' => isset($_GET['posts_per_page']) ? intval($_GET['posts_per_page']) : 10,
-                    'paged' => get_query_var('paged') ? get_query_var('paged') : 1,
+                    'posts_per_page' => $posts_per_page,
+                    'paged' => $paged,
                 );
 
-                if (isset($_GET['title'])) {
+                if (!empty($_GET['title'])) {
                     $args['s'] = sanitize_text_field($_GET['title']);
                 }
 
                 $systems_query = new WP_Query($args);
 
-                if ($systems_query->have_posts()) {
-                    while ($systems_query->have_posts()) {
-                        $systems_query->the_post();
+                if ($systems_query->have_posts()) :
+                    while ($systems_query->have_posts()) : $systems_query->the_post();
                         ?>
                         <tr>
-                            <td><?php the_title(); ?></td>
-                            <td><?php echo get_post_meta(get_the_ID(), 'fqdn', true); ?></td>
-                            <td><?php echo get_post_meta(get_the_ID(), 'operating_system', true); ?></td>
-                            <td><?php echo get_post_meta(get_the_ID(), 'ip_address_lan', true); ?></td>
-                            <td><?php echo get_post_meta(get_the_ID(), 'ip_address_wan', true); ?></td>
-                            <td><?php
-                                $sites = get_the_terms(get_the_ID(), 'sites');
-                                if ($sites && !is_wp_error($sites)) {
-                                    $site_names = wp_list_pluck($sites, 'name');
-                                    echo implode(', ', $site_names);
-                                } else {
-                                    echo 'None';
-                                }
-                            ?></td>
-                            <td><?php echo get_post_meta(get_the_ID(), 'status', true); ?></td>
-                            <td><?php echo get_post_meta(get_the_ID(), 'system_type', true); ?></td>
-                            <td><?php echo get_post_meta(get_the_ID(), 'documentation_links', true); ?></td>
+                            <td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></td>
+                            <td><?php echo esc_html(get_post_meta(get_the_ID(), 'fqdn', true)); ?></td>
+                            <td><?php echo esc_html(get_post_meta(get_the_ID(), 'os', true)); ?></td>
+                            <td><?php echo esc_html(get_post_meta(get_the_ID(), 'lan', true)); ?></td>
+                            <td><?php echo esc_html(get_post_meta(get_the_ID(), 'wan', true)); ?></td>
+                            <td><?php echo esc_html(get_post_meta(get_the_ID(), 'sites', true)); ?></td>
+                            <td><?php echo esc_html(get_post_meta(get_the_ID(), 'status', true)); ?></td>
+                            <td><?php echo esc_html(get_post_meta(get_the_ID(), 'system_type', true)); ?></td>
+                            <td><?php echo esc_html(get_post_meta(get_the_ID(), 'documentation_links', true)); ?></td>
                         </tr>
                         <?php
-                    }
-                } else {
+                    endwhile;
+                    wp_reset_postdata();
+                else :
                     ?>
                     <tr>
-                        <td colspan="9">No systems found</td>
+                        <td colspan="9"><?php _e('No systems found.', 'text_domain'); ?></td>
                     </tr>
                     <?php
-                }
-                wp_reset_postdata();
+                endif;
                 ?>
             </tbody>
         </table>
@@ -78,6 +80,7 @@ function qkn_list_all_systems_page() {
             <?php
             echo paginate_links(array(
                 'total' => $systems_query->max_num_pages,
+                'current' => $paged,
             ));
             ?>
         </div>
